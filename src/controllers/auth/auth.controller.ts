b7bedbbaa6f519494871db7255058453c511b2eb
Monique Lourenco <monique_lourenzia@hotmail.com>
2025-06-30 23:33:33 -0300

    feat: configure Swagger for API documentation

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { ClientProxyService } from '../../service/client-proxy.service';
 import { HttpException } from '../../exceptions/http-exception';
 import { Body, Controller, Post, Res } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiBody, ApiConflictResponse, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOperation, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { CreateUserDto, LoginUserDto } from '../../dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,6 +12,12 @@ export class AuthController {
     ) { }
 
     @Post('store')
+    @ApiOperation({ description: 'Save a new user to the database with the provided data.', summary: 'Create a new user.' })
+    @ApiCreatedResponse({ description: 'Created: The request has been fulfilled and resulted in a new resource being created.' })
+    @ApiBadRequestResponse({ description: 'Bad Request: This response means that the server could not understand the request due to invalid syntax or field.' })
+    @ApiConflictResponse({ description: 'Conflict: The request could not be completed due to a conflict with the current state of the resource.' })
+    @ApiInternalServerErrorResponse({ description: 'Internal Server Error: A generic error message, given when no more specific message is suitable.' })
+    @ApiBody({ type: CreateUserDto, description: 'These fields are mandatory for registration: fullName, email and password.' })
     async store(@Body() body, @Res() res) {
         const response = await this._clientProxyService.client.send('auth/store', body).toPromise();
 
@@ -17,6 +25,13 @@ export class AuthController {
     }
 
     @Post('login')
+    @ApiOperation({ description: 'Authenticate the user with email and password, and return a JWT token upon successful login.', summary: 'User login.' })
+    @ApiCreatedResponse({ description: 'Created: The request has been fulfilled and resulted in a new resource being created.' })
+    @ApiBadRequestResponse({ description: 'Bad Request: This response means that the server could not understand the request due to invalid syntax or field.' })
+    @ApiUnauthorizedResponse({ description: 'Unauthorized: Authentication is needed to get the requested response' })
+    @ApiNotFoundResponse({ description: 'Not Found: The requested resource could not be found' })
+    @ApiInternalServerErrorResponse({ description: 'Internal Server Error: A generic error message, given when no more specific message is suitable.' })
+    @ApiBody({ type: LoginUserDto, description: 'These fields are mandatory for registration: email and password.' })
     async login(@Body() body, @Res() res) {
         const response = await this._clientProxyService.client.send('auth/login', body).toPromise();
 
--- src/controllers/url-shortener/url-shortener.controller.ts
+++ src/controllers/url-shortener/url-shortener.controller.ts
@@ -3,6 +3,8 @@ import { HttpException } from '../../exceptions/http-exception';
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { JwtOptionalGuard } from '../../config/jwt-optional.guard';
 import { JwtGuard } from '../../config/jwt-guard.guard';
+import { UrlDto } from '../../dto/url-sortener.dto';
+import { ApiBadRequestResponse, ApiBody, ApiConflictResponse, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOperation, ApiQuery, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @Controller('url-shortener')
 export class UrlShortenerController {
@@ -13,6 +15,13 @@ export class UrlShortenerController {
 
     @Post('store')
     @UseGuards(JwtOptionalGuard)
+    @ApiOperation({ description: 'Save a new original URL to the database to generate its shortened version.', summary: 'Create a new shortened URL.' })
+    @ApiCreatedResponse({ description: 'Created: The request has been fulfilled and resulted in a new resource being created.' })
+    @ApiBadRequestResponse({ description: 'Bad Request: This response means that the server could not understand the request due to invalid syntax or field.' })
+    @ApiConflictResponse({ description: 'Conflict: The request could not be completed due to a conflict with the current state of the resource.' })
+    @ApiNotFoundResponse({ description: 'Not Found: The requested resource could not be found' })
+    @ApiInternalServerErrorResponse({ description: 'Internal Server Error: A generic error message, given when no more specific message is suitable.' })
+    @ApiBody({ type: UrlDto, description: 'These fields are mandatory for registration: url.' })
     async store(@Body() body, @Res() res, @Req() req) {
         const response = await this._clientProxyService.client.send('url-shortener/store', { body, user: req?.user }).toPromise();
 
@@ -21,6 +30,10 @@ export class UrlShortenerController {
 
     @Get('index')
     @UseGuards(JwtOptionalGuard)
+    @ApiOperation({ description: 'Retrieve all URLs stored in the database. If authenticated with a valid token, returns only the URLs belonging to the authenticated user.', summary: 'List URLs.' })
+    @ApiBadRequestResponse({ description: 'Bad Request: This response means that the server could not understand the request due to invalid syntax or field.' })
+    @ApiNotFoundResponse({ description: 'Not Found: The requested resource could not be found' })
+    @ApiInternalServerErrorResponse({ description: 'Internal Server Error: A generic error message, given when no more specific message is suitable.' })
     async login(@Res() res, @Req() req) {
         const response = await this._clientProxyService.client.send('url-shortener/index', { user: req?.user }).toPromise();
 
@@ -28,6 +41,11 @@ export class UrlShortenerController {
     }
 
     @Get('redirect')
+    @ApiOperation({ summary: 'Redirect to original URL', description: 'Redirects to the original URL based on the shortened version passed as a query parameter.', })
+    @ApiBadRequestResponse({ description: 'Bad Request: This response means that the server could not understand the request due to invalid syntax or field.' })
+    @ApiNotFoundResponse({ description: 'Not Found: The requested resource could not be found' })
+    @ApiInternalServerErrorResponse({ description: 'Internal Server Error: Something went wrong during the redirect process.' })
+    @ApiQuery({ name: 'url', type: String, required: true, description: 'The original URL to be redirected from the shortened version.', })
     async redirect(@Query('url') url: string, @Res() res) {
         const response = await this._clientProxyService.client.send('url-shortener/redirect', { url },).toPromise();
 
@@ -36,6 +54,14 @@ export class UrlShortenerController {
 
     @Put('update/:id')
     @UseGuards(JwtGuard)
+    @ApiOperation({ description: 'Update an existing shortened URL by ID. Authentication is required.', summary: 'Update a shortened URL.' })
+    @ApiCreatedResponse({ description: 'Created: The request has been fulfilled and resulted in a new resource being created.' })
+    @ApiBadRequestResponse({ description: 'Bad Request: This response means that the server could not understand the request due to invalid syntax or field.' })
+    @ApiUnauthorizedResponse({ description: 'Unauthorized: Authentication is needed to get the requested response' })
+    @ApiConflictResponse({ description: 'Conflict: The request could not be completed due to a conflict with the current state of the resource.' })
+    @ApiNotFoundResponse({ description: 'Not Found: The requested resource could not be found' })
+    @ApiInternalServerErrorResponse({ description: 'Internal Server Error: A generic error message, given when no more specific message is suitable.' })
+    @ApiBody({ type: UrlDto, description: 'These fields are mandatory for registration: id and url.' })
     async update(@Body() body, @Param('id') id, @Res() res, @Req() req) {
         const response = await this._clientProxyService.client.send('url-shortener/update', { id, body, user: req?.user }).toPromise();
 
@@ -44,6 +70,13 @@ export class UrlShortenerController {
 
     @Delete('delete/:id')
     @UseGuards(JwtGuard)
+    @ApiOperation({ description: 'Logically deletes the shortened URL by marking it as inactive. The record is no longer visible or editable.', summary: 'Deactivate shortened URL.' })
+    @ApiCreatedResponse({ description: 'Created: The request has been fulfilled and resulted in a new resource being created.' })
+    @ApiBadRequestResponse({ description: 'Bad Request: This response means that the server could not understand the request due to invalid syntax or field.' })
+    @ApiUnauthorizedResponse({ description: 'Unauthorized: Authentication is needed to get the requested response' })
+    @ApiNotFoundResponse({ description: 'Not Found: The requested resource could not be found' })
+    @ApiInternalServerErrorResponse({ description: 'Internal Server Error: A generic error message, given when no more specific message is suitable.' })
+    @ApiBody({ description: 'These fields are mandatory for registration: id.' })
     async delete(@Param('id') id, @Res() res, @Req() req) {
         const response = await this._clientProxyService.client.send('url-shortener/delete', { id, user: req?.user }).toPromise();
 
